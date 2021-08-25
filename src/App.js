f9c27ebbe2d07fb0dace1e85973b9c64fd9bc697
Justin Diaz <justin.s.diaz@gmail.com>
2021-08-25 12:58:56 -0400

    create-react-app with basic card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createGlobalStyle } from 'styled-components'
+import Card from './Card.js'
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #2d2e36;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GlobalStyle/>
+      <Card/>
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Card = () => (
+    <div>
+        
+        <span></span>
+        <h2>An Above Average Experience</h2>
+        <p>Travel to the premiere mountain tops of New Zealand with our guided tours. Take in the majestic scenary and witness beauty.</p>
+
+    </div>
+)
+export default Card
