630c7aa38616ffbe20a600e46605c6c2b967129b
Justin Diaz <justin.s.diaz@gmail.com>
2021-08-25 14:20:27 -0400

    finished basic styling from figma file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
-import './App.css';
+
 import { createGlobalStyle } from 'styled-components'
 import Card from './Card.js'
 
 const GlobalStyle = createGlobalStyle`
+  *,*::before,*::after{
+    box-sizing:border-box;
+  }
+
   body{
-    background: #2d2e36;
+      background: #2d2e36;
+      height: 100vh;
+      display:grid;
+      place-items:center;
+      margin:0;
+      padding:0;
   }
 `
 
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,61 @@
 
 import React from 'react';
+import Styled from 'styled-components'
+import mountain from "./mountain.jpg"
+
+
+const StyledCard = Styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items:center;
+    width:45.5rem;
+    height: 19.3rem;
+    background: #383A45;
+    padding: 1rem;
+    border-radius: 0.5rem;
+
+
+    div{
+        max-width: 22.4375rem;
+    }
+
+    img{
+        width: 262px;
+        height: 250px;
+        border-radius: 8px;
+    }
+    h2{
+        color:white;
+        font-family: Poppins;
+        font-size: 30px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 36px;
+        letter-spacing: 0em;
+        text-align: left;
+
+        
+    }
+    p{
+        color: #8D91A6;
+        font-family: Poppins;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px;
+        letter-spacing: 0em;
+        text-align: left;
+        
+    }
+`
 
 const Card = () => (
-    <div>
-        
-        <span></span>
-        <h2>An Above Average Experience</h2>
-        <p>Travel to the premiere mountain tops of New Zealand with our guided tours. Take in the majestic scenary and witness beauty.</p>
-
-    </div>
+    <StyledCard>
+        <img src={mountain} alt="a mountain"/>
+        <div>
+            <h2>An Above Average Experience</h2>
+            <p>Travel to the premiere mountain tops of New Zealand with our guided tours. Take in the majestic scenary and witness beauty.</p>
+        </div>
+    </StyledCard>
 )
 export default Card
